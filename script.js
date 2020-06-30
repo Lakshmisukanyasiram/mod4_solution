d9eb2cc95afb42cab308b32c587e4d0230888819
Lakshmisukanyasiram <64964032+Lakshmisukanyasiram@users.noreply.github.com>
2020-06-30 19:04:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -32,14 +32,15 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (var i=0,i<names.length;i++) {
+for (var i=0,i < names.length;i++) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
